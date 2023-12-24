cd506bc438d5c27cddea1a7632a8d356dcd0e688
Kit <haosenya@outlook.com>
2023-12-24 10:53:02 +0800

    1.0.0

---
+++ BankManage/BankManage/utils/FileUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+
+/// <summary>
+/// 文件是否占用
+/// </summary>
+/// <param name="fileName"></param>
+/// <returns></returns>
+
+namespace BankManage.utils {
+    internal class FileUtils {
+        public static bool IsFileInUse(string fileName) {
+            bool inUse = true;
+
+            FileStream fs = null;
+            try {
+
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
+
+                inUse = false;
+            } catch {
+            } finally {
+                if (fs != null)
+
+                    fs.Close();
+            }
+            return inUse;//true表示正在使用,false没有使用
+        }
+    }
+}
---
+++ BankManage/BankManage/utils/LogHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+using log4net;
+using log4net.Config;
+
+namespace BankManage.utils {
+    internal class LogHelper {
+        public static readonly ILog Loginfo = LogManager.GetLogger("loginfo");
+        public static readonly ILog Logerror = LogManager.GetLogger("logerror");
+
+        public static void InitLog4Net() {
+            FileInfo logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            XmlConfigurator.ConfigureAndWatch(logCfg);
+        }
+
+        /// <summary>
+        /// 停止日志记录
+        /// </summary>
+        public static void StopLog() {
+            //应用结束前要上传日志文件，我们就要停止日志，防止文件占用
+            log4net.LogManager.Shutdown();
+        }
+    }
+}
