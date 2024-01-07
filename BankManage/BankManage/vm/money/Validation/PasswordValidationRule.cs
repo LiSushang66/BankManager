81dc989b78a833ea60664bb8e67e6e6fa0a76fb4
Cheater6277 <96470294+Cheater6277@users.noreply.github.com>
2024-01-07 17:47:20 +0800

    密码输入提示

--- BankManage/BankManage/vm/money/Validation/PasswordValidationRule.cs
+++ BankManage/BankManage/vm/money/Validation/PasswordValidationRule.cs
@@ -9,7 +9,7 @@ namespace BankManage.vm.money.Validation
         private static readonly string PASSWORD_STRENGTH = @"(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*\d))(?=(.*[ !""#$%&'()*+,-./:;<=>?@\[\]\^_`{|}~]))^.{8,32}$";
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            return new Regex(PASSWORD_STRENGTH).IsMatch(value.ToString()) ? new ValidationResult(true, null) : new ValidationResult(false, "密码格式不正确");
+            return new Regex(PASSWORD_STRENGTH).IsMatch(value.ToString()) ? new ValidationResult(true, null) : new ValidationResult(false, "密码格式不正确, 需要大小写字符各至少一个, 特殊字符例如'@'一个,密码长度至少为8");
         }
     }
 }
